272a82fd2e421e63bc407175e527954419c0fc9d
Svetlana Prokopeva <lana.prokopeva@yandex.ru>
2022-09-17 20:04:39 +0300

    Task001 added

---
+++ Task001/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите ЦИКЛ, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Решить задачу с использованием методов.
+
+// Возводим число A в натуральную степень B
+double GetPowNum(int a, int b)
+{
+    double result = a;
+    if (b == 0)
+    {
+        result = 1;
+    }
+    else if (b < 0)
+    {
+        result = Math.Pow(a, b);
+    }
+    else
+    {
+        for (int i = 2; i <= b; i++)
+        {
+            result = result * a;
+        } 
+    }
+    return result;
+}
+
+// Определяем входные числа
+Console.WriteLine("Введите число А: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число В: ");
+int B = int.Parse(Console.ReadLine());
+
+double powNum = GetPowNum(A, B);
+Console.WriteLine($"{A} в степени {B} = {powNum}");
